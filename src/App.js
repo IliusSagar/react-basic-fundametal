d4dc52201923316782961f83216b23ddca6172b3
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-26 20:01:37 +0600

    stateless component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ClassTest from './components/ClassTest';
 import GreetFunctional from './components/GreetFunctional';
+import Stateless from './components/Stateless';
 
 function App() {
   return (
     <div className="App">
       <ClassTest />
     <GreetFunctional/>
+    <Stateless name="Sagar" email="iliussagar@gmail.com" phone="8801830596312"/>
     </div>
   );
 }
---
+++ src/components/Stateless.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Stateless = (props) => {
+    const {name,email,phone} = props;
+    return(
+        <div>
+            <h3>Teacher's Details</h3>
+            <p>Name : {name}</p>
+            <p>Email : {email}</p>
+            <p>Phone : {phone}</p>
+        </div>
+    )
+}
+
+export default Stateless;
