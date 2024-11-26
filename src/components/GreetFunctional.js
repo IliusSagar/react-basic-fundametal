4628558e1914b44bbbed53bfe707d51617ff7b82
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-26 14:12:31 +0600

    functional Components

---
+++ src/components/GreetFunctional.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function GreetFunctional(){
+//     return <h1>Hello Functional Components</h1>
+// }
+
+const GreetFunctional = () => <h1>Hello Functions</h1>
+
+export default GreetFunctional
