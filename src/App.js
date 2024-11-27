b2dc5e0e0addfb62404f2eb2192297be9ef9dbb8
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-27 11:18:31 +0600

    props functional component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GreetFunctional from './components/GreetFunctional';
 import Stateless from './components/Stateless';
 import Stateful from './components/Stateful';
 import Hello from './components/Hello';
+import Greet from './components/Greet';
 
 function App() {
   return (
@@ -14,7 +15,15 @@ function App() {
     <Stateless name="Sagar" email="iliussagar@gmail.com" phone="8801830596312"/>
     <Stateful name="Sagar1" email="iliussagar1@gmail.com" phone="8801730596312"/> */}
 
-    <Hello />
+    {/* <Hello /> */}
+
+    <Greet name="Bruce" heroName="Batman">
+      <p>This is children props</p>
+    </Greet>
+    <Greet name="Clark" heroName="Superman">
+      <button>Action</button>
+    </Greet>
+    <Greet name="Diana" heroName="Wonder woman"/>
 
     
     </div>
